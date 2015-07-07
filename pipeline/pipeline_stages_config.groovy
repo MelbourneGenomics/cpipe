5a8ac35e92267e0d753407af2235bcd9cafa0faf
Alejandro Dubrovsky <Alex.Dubrovsky@agrf.org.au>
2015-07-07 12:23:22 +1000

    The three-parameter variant of the match function is a gawk extension. Convert the check to use standard awk

--- pipeline/pipeline_stages_config.groovy
+++ pipeline/pipeline_stages_config.groovy
@@ -317,7 +317,7 @@ check_fastqc = {
 
     check("FASTQ Format") {
         exec """
-            awk -F'\\t' '/Illumina/ { match(\$2, /[0-9.]+/ , result); exit(result[0]<1.7) }' fastqc/${sample}_*_fastqc/fastqc_data.txt
+            awk -F'\\t' '/Illumina/ { where=match(\$2, /[0-9.]+/); { result=substr(\$2, where, RLENGTH); exit(result<1.7); } }' fastqc/${sample}_*_fastqc/fastqc_data.txt
         ""","local"
     } otherwise {
         println "=" * 100
