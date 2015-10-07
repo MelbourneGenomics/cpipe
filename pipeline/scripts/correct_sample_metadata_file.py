acd4c122d3a0b492adf2c73796f58ff2b2540080
Peter Georgeson <peter@supernifty.org>
2015-10-07 19:01:21 +1100

    fixed source header

--- pipeline/scripts/correct_sample_metadata_file.py
+++ pipeline/scripts/correct_sample_metadata_file.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
-####################################################################################
+###########################################################################
 #
-# Melbourne Genomics Pipeline Annotation Script
+# This file is part of Cpipe.
 #
-# Copyright Melbourne Genomics Health Alliance members. All rights reserved.
+# Cpipe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, under version 3 of the License, subject
+# to additional terms compatible with the GNU General Public License version 3,
+# specified in the LICENSE file that is part of the Cpipe distribution.
 #
-# DISTRIBUTION:
+# Cpipe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# This source code should not be distributed to a third party without prior
-# approval of the Melbourne Genomics Health Alliance steering committee (via
-# Natalie Thorne - natalie.thorne@melbournegenomics.org.au).
+# You should have received a copy of the GNU General Public License
+# along with Cpipe.  If not, see <http:#www.gnu.org/licenses/>.
 #
-####################################################################################
+###########################################################################
 #
 # Purpose:
 # * remove spaces intelligently from the priority gene list column
