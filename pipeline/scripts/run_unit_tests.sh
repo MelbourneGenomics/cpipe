b6e5c2ada74c75bf0f7fb95faa57645aff5f1f8f
Peter Georgeson <peter@supernifty.org>
2015-12-15 13:09:10 +1100

    generate xml test result with RunAll.groovy

--- pipeline/scripts/run_unit_tests.sh
+++ pipeline/scripts/run_unit_tests.sh
@@ -27,7 +27,7 @@
 export GROOVY="$PWD/tools/groovy/2.3.4/bin/groovy"
 export GROOVY_NGS="$PWD/tools/groovy-ngs-utils/1.0.2"
 export EXCEL="$PWD/tools/excel/1.0"
-GROOVY_TEST_LIBRARIES="$PWD/tools/groovy/2.3.4/lib/groovy-2.3.4.jar:$PWD/tools/groovy/2.3.4/lib/hamcrest-core-1.3.jar:$PWD/tools/groovy/2.3.4/lib/junit-4.11.jar:$PWD/pipeline/tests/lib/cpsuite-1.2.6.jar:$EXCEL/excel.jar:$GROOVY_NGS/groovy-ngs-utils.jar"
+GROOVY_TEST_LIBRARIES="$PWD/tools/groovy/2.3.4/lib/groovy-2.3.4.jar:$PWD/tools/groovy/2.3.4/lib/hamcrest-core-1.3.jar:$PWD/tools/groovy/2.3.4/lib/junit-4.11.jar:$PWD/pipeline/tests/lib/cpsuite-1.2.6.jar:$EXCEL/excel.jar:$GROOVY_NGS/groovy-ngs-utils.jar:$PWD/pipeline/tests/lib/JUnitXmlFormatter.jar"
 GROOVYC="$PWD/tools/groovy/2.3.4/bin/groovyc"
 
 # python tests
