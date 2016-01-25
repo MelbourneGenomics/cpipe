ee17ad33cca88f5eb2f4a752fc7c8bea6c139683
Peter Georgeson <peter@supernifty.org>
2016-01-25 21:32:05 +1100

    correct qc_report unit tests

--- pipeline/tests/qc_report_test.py
+++ pipeline/tests/qc_report_test.py
@@ -33,7 +33,7 @@ import qc_report
 class QCReportTest(unittest.TestCase):
 
     def test_male(self):
-        exome_cov = ['chr1\t100\t200\tA\t1\t20', 'chrX\t100\t200\tA\t1\t10', 'chrY\t100\t200\tA\t1\t10']
+        exome_cov = ['chr1\t100\t200\t1\t20', 'chrX\t100\t200\t1\t10', 'chrY\t100\t200\t1\t10']
         log = StringIO.StringIO()
         result = qc_report.calculate_karyotype(exome_cov, log)
         print result
@@ -43,7 +43,7 @@ class QCReportTest(unittest.TestCase):
         assert result['autosome_mean_coverage'] == 20.
 
     def test_female(self):
-        exome_cov = ['chr1\t100\t200\tA\t1\t5', 'chrX\t100\t200\tA\t1\t40', 'chrX\t400\t500\tA\t1\t60']
+        exome_cov = ['chr1\t100\t200\t1\t5', 'chrX\t100\t200\t1\t40', 'chrX\t400\t500\t1\t60']
         log = StringIO.StringIO()
         result = qc_report.calculate_karyotype(exome_cov, log)
         print result
