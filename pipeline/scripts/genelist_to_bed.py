c29ab1d9b0e927d7a128aacab9ad3c137478e3b0
Peter Georgeson <peter@supernifty.org>
2016-06-28 15:31:40 +1000

    pylint fixes

--- pipeline/scripts/genelist_to_bed.py
+++ pipeline/scripts/genelist_to_bed.py
@@ -32,23 +32,23 @@
 
 import sys
 
-def filter_bed( genelists, bed_in, bed_out, log, exclude=None ):
+def filter_bed(genelists, bed_in, bed_out, log, exclude=None):
     ''' get the list of proposed genes '''
     genes = set()
     for arg in genelists:
-        for line in open( arg, 'r' ):
+        for line in open(arg, 'r'):
             if line.startswith('#'):
                 continue
-            fields = line.strip().split( '\t' )
-            genes.add( fields[0].upper() )
-    log.write( '%i candidate genes added\n' % len(genes) )
-    
+            fields = line.strip().split('\t')
+            genes.add(fields[0].upper())
+    log.write('%i candidate genes added\n' % len(genes))
+
     # get the list of exclusions
     disallowed = set()
     if exclude is not None:
         for line in exclude:
-            disallowed.add( line.strip().upper() )
-        log.write( '%i excluded genes added\n' % len(disallowed) )
+            disallowed.add(line.strip().upper())
+        log.write('%i excluded genes added\n' % len(disallowed))
 
     # filter the reference
     filtered = 0
@@ -57,39 +57,44 @@ def filter_bed( genelists, bed_in, bed_out, log, exclude=None ):
     blocked = set()
     for line in bed_in:
         if line.startswith('#'):
-            bed_out.write( line )
+            bed_out.write(line)
             allowed += 1
         else:
-            fields = line.strip().split( '\t' )
+            fields = line.strip().split('\t')
             if len(fields) > 3:
                 candidate = fields[3].upper()
                 if candidate in genes:
                     if candidate in disallowed:
-                        blocked.add( candidate )
+                        blocked.add(candidate)
                     else:
-                        bed_out.write( line )
+                        bed_out.write(line)
                         allowed += 1
-                        found.add( candidate )
+                        found.add(candidate)
                 else:
                     filtered += 1
             else:
-                bed_out.write( line )
+                bed_out.write(line)
                 allowed += 1
-    
-    log.write( '%i lines written, %i lines filtered, %i out of %i candidate genes found\n' % ( allowed, filtered, len(found), len(genes) ) )
-    if len(found) != len(genes):
-        log.write( 'Not found: %s\n' % ' '.join( sorted( list( genes.difference( found.union( blocked ) ) ) ) ) )
-    if len(blocked) > 0:
-        log.write( 'Excluded: %s\n' % ' '.join( sorted( list( blocked ) ) ) )
 
-if __name__ == '__main__':
+    log.write('%i lines written, %i lines filtered, %i out of %i candidate genes found\n' % (allowed, filtered, len(found), len(genes)))
+    if len(found) != len(genes):
+        log.write('Not found: %s\n' % ' '.join(sorted(list(genes.difference(found.union(blocked))))))
+    if len(blocked) > 0:
+        log.write('Excluded: %s\n' % ' '.join(sorted(list(blocked))))
+
+def main():
+    '''
+        run from command line
+    '''
     import argparse
     parser = argparse.ArgumentParser(description='Convert genelists to bed file')
     parser.add_argument('--exclude', dest='exclude', required=False, help='file containing genes to exclude')
-    parser.add_argument('genelists', nargs='*', help='gene list files to include' )
+    parser.add_argument('genelists', nargs='*', help='gene list files to include')
     args = parser.parse_args()
     if args.exclude:
-        filter_bed( args.genelists, sys.stdin, sys.stdout, sys.stderr, exclude=open( args.exclude, 'r' ) )
+        filter_bed(args.genelists, sys.stdin, sys.stdout, sys.stderr, exclude=open(args.exclude, 'r'))
     else:
-        filter_bed( args.genelists, sys.stdin, sys.stdout, sys.stderr )
+        filter_bed(args.genelists, sys.stdin, sys.stdout, sys.stderr)
 
+if __name__ == '__main__':
+    main()
