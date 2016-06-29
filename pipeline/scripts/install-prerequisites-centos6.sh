64e77318e49083b01b2ba0b6c0cfa4e16a6e0a55
Peter Georgeson <peter@supernifty.org>
2016-06-29 16:11:16 +1000

    centos prerequisites documentation

--- pipeline/scripts/install-prerequisites-centos6.sh
+++ pipeline/scripts/install-prerequisites-centos6.sh
@@ -3,6 +3,9 @@
 #
 # Installs Centos 6 dependencies
 #
+# this is a set of recommended commands to run in preparation
+# for Cpipe.
+# evaluate for yourself if you want to run them on your system.
 ############################################################
 
 # software
@@ -14,11 +17,6 @@ sudo yum install java-1.8.0-openjdk zlib-devel ncurses-devel gcc-c++ R patch
 echo "export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java" >> ~/.bash_profile
 . ~/.bash_profile
 
-# vep and perl
-sudo yum install perl-core perl-DBD-MySQL
-sudo perl -MCPAN -e "install HTTP::Tiny"
-sudo perl -MCPAN -e "install LWP::Simple"
-
 # perl too old?
 curl -L https://install.perlbrew.pl | bash
 echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
@@ -26,3 +24,11 @@ echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
 perlbrew install-patchperl
 perlbrew install perl-5.18.1
 perlbrew switch perl-5.18.1
+
+# vep and perl
+sudo yum install perl-core perl-DBD-MySQL
+
+# don't need sudo if using perlbrew
+cpan -e "install HTTP::Tiny"
+#cpan -e "install LWP::Simple"
+cpan -e "install Archive::Zip"
