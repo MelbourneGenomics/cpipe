e5e4f3764dc8ce2292d6d957bbb0fa1fe3db8f32
root <root@milton-mgha-test.novalocal>
2016-08-02 07:01:28 +0000

    Use sudo session

--- install/dependencies_ubuntu.sh
+++ install/dependencies_ubuntu.sh
@@ -3,16 +3,17 @@
 # Installs all dependencies for the installer and pipeline on Ubuntu
 
 #Fix docker issues
-sudo mkdir /etc/systemd/system/docker.service.d/
-'[Service]
+sudo -s
+mkdir /etc/systemd/system/docker.service.d/
+echo '[Service]
 ExecStart=
 ExecStart=/usr/bin/docker daemon -H fd:// -s overlay' >> /etc/systemd/system/docker.service.d/overlay.conf
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 # Install gradle repos
-sudo add-apt-repository ppa:cwchien/gradle
-sudo apt-get update
+add-apt-repository ppa:cwchien/gradle
+apt-get update
 
 # Install apt-getable things
-sudo apt-get install -y git make gcc poppler-utils zlib1g-dev ncurses-dev g++ patch libssl-dev unzip maven\
+apt-get install -y git make gcc poppler-utils zlib1g-dev ncurses-dev g++ patch libssl-dev unzip maven\
  gradle libcurl4-openssl-dev texinfo openjdk-8-jdk
