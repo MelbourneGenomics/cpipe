2e4c588d3de9257445303f0b15d5bd271bf2f232
Ubuntu <ubuntu@milton-mgha-test.novalocal>
2016-08-02 02:24:38 +0000

    Remove dependencies that are now assets

--- install/dependencies_ubuntu.sh
+++ install/dependencies_ubuntu.sh
@@ -1,22 +1,7 @@
 # Installs all dependencies for the installer and pipeline
 # Note: Only for Ubuntu 16.04
 
-PYTHON_VERSION=2.7.12
-PERL_VERSION=5.24.0
-R_VERSION=3.3.1-1xenial0
 JAVA_VERSON=8u91-b14-0ubuntu4~16.04.1
-
-# Install python using pyenv
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="/home/ubuntu/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)" >> ~/.bash_profile
-pyenv install $PYTHON_VERSION
-
-# Install perl using perlbrew
-curl -L https://install.perlbrew.pl | bash
-echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
-perlbrew install perl-$PERL_VERSION
 
 #Fix docker issues
 sudo mkdir /etc/systemd/system/docker.service.d/
@@ -25,14 +10,12 @@ ExecStart=
 ExecStart=/usr/bin/docker daemon -H fd:// -s overlay' >> /etc/systemd/system/docker.service.d/overlay.conf
 sudo systemctl daemon-reload
 
-#Install R using CRAN repository
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sudo add-apt-repository 'deb http://cran.ms.unimelb.edu.au/bin/linux/ubuntu xenial/'
-sudo apt-get update
-sudo apt-get install -y r-base=$R_VERSION\*
-
 # Install Java
-sudo apt-get install -y openjdk-8-jre=$JAVA_VERSION\*
+sudo apt-get install -y openjdk-8-jdk=$JAVA_VERSION\*
+
+# Install gradle repos
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
 
 # Install apt-getable things
-sudo apt-get install -y git make gcc poppler-utils zlib1g-dev ncurses-dev g++ patch libssl-dev unzip maven
+sudo apt-get install -y git make gcc poppler-utils zlib1g-dev ncurses-dev g++ patch libssl-dev unzip maven gradle
