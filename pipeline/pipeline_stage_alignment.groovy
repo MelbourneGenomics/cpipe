ddbc3062e0685a67b9405e3abb9a942e8563a8da
supernifty <peter@supernifty.org>
2016-08-30 01:36:06 +0000

    indelrealigner and printreads doesn't require -L as outlined by GATK best practice

--- pipeline/pipeline_stage_alignment.groovy
+++ pipeline/pipeline_stage_alignment.groovy
@@ -302,7 +302,7 @@ realign = {
              -T IndelRealigner 
              -R $REF 
              -I $input.bam 
-             -L $COMBINED_TARGET --interval_padding $INTERVAL_PADDING_CALL
+             -L $COMBINED_TARGET
              -targetIntervals $input.intervals 
              -o $output.bam
     ""","local_realign"
@@ -336,7 +336,7 @@ recal = {
                -T PrintReads 
                -I $input.bam 
                -BQSR $input.counts 
-               -L $COMBINED_TARGET --interval_padding $INTERVAL_PADDING_CALL
+               -L $COMBINED_TARGET 
                -R $REF 
                -l INFO 
                -o $output.bam
@@ -380,7 +380,7 @@ legacy_recal = {
             exec """
                 $JAVA -Xmx3g -jar $GATK/GenomeAnalysisTK.jar
                     -l INFO
-                    -L $COMBINED_TARGET --interval_padding $INTERVAL_PADDING_CALL
+                    -L $COMBINED_TARGET
                     -R $REF
                     -I $input.bam
                     -T PrintReads
