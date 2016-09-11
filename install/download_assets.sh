9d15f41bdbfc4d7f8d0989c88006f39d5f57288e
root <ttmigueltt@gmail.com>
2016-09-11 07:37:00 +0000

    Use correct commit for vep_plugins

--- install/download_assets.sh
+++ install/download_assets.sh
@@ -22,7 +22,7 @@ CPSUITE_VERSION="1.2.7"
 FASTQC_VERSION="0.11.5"
 PICARD_VERSION="2.6.0"
 DBNSFP_VERSION="2.9.1" # Use the latest v2 version. v3 of dbNSFP uses HG38
-VEP_PLUGIN_COMMIT="caa2008"
+VEP_PLUGIN_COMMIT="3be3889"
 
 ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]})/..) #The cpipe root directory
 TOOLS_ROOT=$ROOT/tools
