98ee2a5a4bcf96d8d643b47e47e2cb4cafd99c99
Michael Milton <ttmigueltt@gmail.com>
2016-09-22 16:37:14 +1000

    Add trio refinement download

--- install/download_assets.sh
+++ install/download_assets.sh
@@ -303,6 +303,10 @@ function download_list {
             mkdir -p $TOOLS_ROOT/picard\
             && wget -P $TOOLS_ROOT/picard https://github.com/broadinstitute/picard/releases/download/2.6.0/picard.jar >> $LOG_FILE
             check_success
+
+            echo -n 'Compiling picard...' \
+            compile $TOOLS_ROOT/picard >> $LOG_FILE
+            check_success
         else
             echo 'already satisfied'
         fi
@@ -397,12 +401,34 @@ function download_list {
         "dbsnp_138.hg19.vcf.gz dbsnp_138.hg19.vcf.idx.gz" \
         $DATA_ROOT/dbsnp
 
-        download_list \
-        ftp://ftp.broadinstitute.org/bundle/2.8/b37/ \
-        gsapubftp-anonymous:cpipe.user@cpipeline.org \
-        "1000G_phase3_v4_20130502.sites.vcf.gz 1000G_phase3_v4_20130502.sites.vcf.gz.tbi" \
-        $DATA_ROOT/1000G_phase3
+        if ! fileExists ${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.hg19.vcf.gz ; then
+            echo -n 'Downloading the trio refinement reference...' \
+            mkdir -p ${DATA_ROOT}/1000G_phase3\
+            && curl \
+                --user gsapubftp-anonymous:cpipe.user@cpipeline.org \
+                 ftp://ftp.broadinstitute.org/bundle/2.8/b37/1000G_phase3_v4_20130502.sites.vcf.gz \
+                 | gunzip > ${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.vcf
+            check_success
 
+            echo -n 'Downloading the trio refinement liftover file...' \
+            mkdir -p ${DATA_ROOT}/1000G_phase3\
+            && wget
+                --user gsapubftp-anonymous:cpipe.user@cpipeline.org
+                 -P ${DATA_ROOT}/1000G_phase3 \
+                 ftp://gsapubftp-anonymous@ftp.broadinstitute.org/Liftover_Chain_Files/b37tohg19.chain \
+            check_success
+
+            echo -n 'Converting and indexing the trio refinement reference...' \
+            && java -jar $TOOLS_ROOT/picard/picard.jar LiftoverVcf \
+                I=${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.vcf \
+                O=${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.hg19.vcf \
+                CHAIN=${DATA_ROOT}/1000G_phase3/b37tohg19.chain \
+                REJECT=${DATA_ROOT}/1000G_phase3/liftover.rejected_variants.vcf \
+                R=${DATA_ROOT}/ucsc/ucsc.hg19.fasta\
+            && bgzip ${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.hg19.vcf \
+            && tabix -p vcf ${DATA_ROOT}/1000G_phase3/1000G_phase3_v4_20130502.sites.hg19.vcf.gz
+            check_success
+        fi
 
         echo -n 'Downloading chromosome sizes...'
         if ! fileExists $DATA_ROOT/chromosomes/hg19.genome ; then
