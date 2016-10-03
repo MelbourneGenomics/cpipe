1f1314f7023e9ab4646651d5f4c7b3cc2055d9b6
Michael Milton <ttmigueltt@gmail.com>
2016-10-03 16:54:48 +1100

    Make download a dependency on compile

--- tasks/compile_tools.py
+++ tasks/compile_tools.py
@@ -20,6 +20,7 @@ def task_compile_perl():
         'actions': [
             cmd('yes | ./configure.sh && make', cwd=PERL_ROOT)
         ],
+        'task_dep': ['download_perl'],
         'targets': [os.path.join(PERL_ROOT, 'perl')],
         'uptodate': [True]
     }
@@ -29,6 +30,7 @@ def task_compile_r():
         'actions': [
             cmd('./configure && make', cwd=R_ROOT)
         ],
+        'task_dep': ['download_r'],
         'targets': [os.path.join(R_ROOT, 'bin', 'R')],
         'uptodate': [True]
     }
@@ -38,6 +40,7 @@ def task_compile_bwa():
         'actions': [
             cmd('./configure && make', cwd=BWA_ROOT)
         ],
+        'task_dep': ['download_bwa'],
         'targets': [os.path.join(BWA_ROOT, 'bwa')],
         'uptodate': [True]
     }
@@ -47,6 +50,7 @@ def task_compile_htslib():
         'actions': [
             cmd('make', cwd=HTSLIB_ROOT)
         ],
+        'task_dep': ['download_htslib'],
         'targets': [os.path.join(HTSLIB_ROOT, 'htsfile')],
         'uptodate': [True]
     }
@@ -56,6 +60,7 @@ def task_compile_samtools():
         'actions': [
             cmd('make', cwd=SAMTOOLS_ROOT)
         ],
+        'task_dep': ['download_samtools'],
         'targets': [os.path.join(SAMTOOLS_ROOT, 'samtools')],
         'uptodate': [True]
     }
@@ -65,6 +70,7 @@ def task_compile_bcftools():
         'actions': [
             cmd('make', cwd=BCFTOOLS_ROOT)
         ],
+        'task_dep': ['download_bcftools'],
         'targets': [os.path.join(BCFTOOLS_ROOT, 'bcftools')],
         'uptodate': [True]
     }
@@ -74,6 +80,7 @@ def task_compile_bedtools():
         'actions': [
             cmd('make', cwd=BEDTOOLS_ROOT)
         ],
+        'task_dep': ['download_bedtools'],
         'targets': [os.path.join(BEDTOOLS_ROOT, 'bedtools')],
         'uptodate': [True]
     }
@@ -89,6 +96,7 @@ def task_compile_gatk():
             && bash -O extglob -c 'rm -rf !(GenomeAnalysisTK.jar)'
             ''', cwd=GATK_ROOT)
         ],
+        'task_dep': ['download_gatk'],
         'targets': [os.path.join(GATK_ROOT, 'gatk')],
         'uptodate': [True]
     }
