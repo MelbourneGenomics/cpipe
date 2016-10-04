bc0e66b1b628bd0a6c0ebe02364f66d2a8eb5741
Michael Milton <ttmigueltt@gmail.com>
2016-10-04 14:58:38 +1100

    Load swift credentials if they exist

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ ROOT=$(readlink -f $(dirname ${BASH_SOURCE}))
 PYTHON=${ROOT}/tools/python
 PYBIN=${PYTHON}/bin
 
+# Load swift credentials if they exist
+if [[ -f ${ROOT}/swift_credentials.sh ]] ; then
+    source ${ROOT}/swift_credentials.sh
+fi
+
 # Use python-build to install python
 if [[ ! -d ${PYTHON} ]]; then
 	pushd tmpdata
@@ -27,4 +32,3 @@ ${PYBIN}/python -m doit install
 
 # Export the python location so this can be source'd
 export PATH=${PYBIN}:$PATH
-
