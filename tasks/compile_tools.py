073b824c47c3799aa0a256bd3bdca116420ce1e2
Michael Milton <ttmigueltt@gmail.com>
2016-10-07 14:55:09 +1100

    Fix perl depedency bug

--- tasks/compile_tools.py
+++ tasks/compile_tools.py
@@ -139,7 +139,7 @@ def task_install_perl_libs():
     """
     return {
         'targets': [os.path.join(PERL_LIB_ROOT, 'bin')],
-        'task_dep': 'download_nectar_assets' if has_swift_auth() else ['download_perl_libs', 'download_cpanm'],
+        'task_dep': ['download_nectar_assets'] if has_swift_auth() else ['download_perl_libs', 'download_cpanm'],
         'actions': [
             # Use the cpan directory we made in download_perl_libs as a cpan mirror and install from there
             cmd('cpanm -l {perl_lib} --mirror file://{tools_dir}/cpan --installdeps .'.format(tools_dir=TOOLS_ROOT, perl_lib=PERL_LIB_ROOT), cwd=INSTALL_ROOT)
