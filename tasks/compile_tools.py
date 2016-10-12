32d09aa52aad6ce9084c5bf9c346a913e88326d9
Michael Milton (University of Melbourne) <milton@merri.pcf.vlsci.unimelb.edu.au>
2016-10-12 16:50:14 +1100

    Compile perl properly

--- tasks/compile_tools.py
+++ tasks/compile_tools.py
@@ -42,7 +42,12 @@ def task_compile_perl():
 
     return {
         'actions': [
-            cmd('yes | ./Configure -de -Dprefix={} && make && make install'.format(C_INCLUDE_ROOT), cwd=PERL_ROOT)
+            cmd('''
+                  ./Configure -de -Dprefix={} 
+                  make
+                  make install
+                '''.format(C_INCLUDE_ROOT), cwd=PERL_ROOT
+            )
         ],
         'task_dep': ['download_nectar_assets' if has_swift_auth() else 'download_perl'],
         'targets': [os.path.join(PERL_ROOT, 'perl')],
