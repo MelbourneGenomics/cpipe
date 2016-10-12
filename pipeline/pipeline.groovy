b83f66e1f2d88492e04ce94ee92d2d0399f2dc05
Michael Milton (University of Melbourne) <milton@merri.pcf.vlsci.unimelb.edu.au>
2016-10-12 22:06:06 +1100

    Don't update sample database

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -1,4 +1,3 @@
-// vim: ts=4:sw=4:expandtab:cindent
 /////////////////////////////////////////////////////////////////////////////////
 //
 // This file is part of Cpipe.
@@ -203,11 +202,11 @@ run {
    [
        proband_samples *
        [
-           set_sample_name_without_target + set_analysis_type_trio + update_sample_database
+           set_sample_name_without_target + set_analysis_type_trio// + update_sample_database
        ],
        individual_samples * // individuals and probands
        [
-           set_sample_name_without_target + set_analysis_type_individual + update_sample_database
+           set_sample_name_without_target + set_analysis_type_individual// + update_sample_database
        ]
    ]
    
