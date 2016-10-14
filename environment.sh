5e463a6f705a7b85d5738fb9c789471dca0b0b9b
Michael Milton (University of Melbourne) <milton@merri.pcf.vlsci.unimelb.edu.au>
2016-10-14 15:34:15 +1100

    Stop java infinite loop

--- environment.sh
+++ environment.sh
@@ -21,6 +21,7 @@ CROOT=${TOOLS}/c_libs
 source ${TOOLS}/python/bin/activate
 
 # Add all tool directories and bin folders to PATH
+export SYS_JAVA=`which $JAVA` # Export the old system java before we override it 
 export PATH=`join ':' $TOOLS/*/`:`join ':' $TOOLS/*/bin/`:$PATH
 export HTSLIB_DIR=$TOOLS/htslib
 export PERL5LIB=$TOOLS/perl_lib/lib/perl5:$TOOLS/perl/lib:$TOOLS/vep_plugins:${TOOLS}/vep_libs
@@ -29,4 +30,4 @@ export CFLAGS="$CFLAGS -I${TOOLS}/c_libs/include"
 export CPPFLAGS="$CPPFLAGS -I${TOOLS}/c_libs/include"
 export LDFLAGS="$LDFLAGS -L${TOOLS}/c_libs/lib"
 export LD_LIBRARY_PATH=${TOOLS}/c_libs/lib:${LD_LIBRARY_PATH}
-export $JAVA_OPTS #Pass JAVA_OPTS to the script in c_libs/bin/java
+export JAVA_OPTS #Pass JAVA_OPTS to the script in c_libs/bin/java
