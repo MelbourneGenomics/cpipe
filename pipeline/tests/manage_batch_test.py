c90afb90972b55ff6a2047126746a4d561257085
Michael Milton <ttmigueltt@gmail.com>
2016-11-01 16:18:38 +1100

    Fixed and added test for manage batch script

--- pipeline/tests/manage_batch_test.py
+++ pipeline/tests/manage_batch_test.py
@@ -1,19 +1,83 @@
 import os
 import unittest
 import sys
-
-from manage_batch import add_batch
-from cpipe_utilities import BASE
+import traceback
+import StringIO
+import shutil
+from contextlib import contextmanager
 
 sys.path.append('../scripts/')
 
+from manage_batch import add_batch, show_batches, show_batch, add_sample
+from cpipe_utility import BASE
+
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
 class ManageBatchTest(unittest.TestCase):
+    test_batch_name = 'test'
+    test_data_dir = os.path.join(BASE, 'pipeline/tests/detect_mutations_test/data')
+    test_data_files = [os.path.join(test_data_dir, f) for f in os.listdir(test_data_dir)]
+    test_batch_dir = os.path.join(BASE, 'batches', test_batch_name)
+    batch_groovy = os.path.join(test_batch_dir, 'config.batch.groovy')
+    sample_metadata = os.path.join(test_batch_dir, 'samples.txt')
+
+    def create_test_batch(self):
+        add_batch(self.test_batch_name, 'ALL', None, self.test_data_files, None, log=sys.stdout)
+
+    def delete_test_batch(self):
+        batch_dir = os.path.join(BASE, 'batches', self.test_batch_name)
+        shutil.rmtree(batch_dir)
+
     def test_add_batch(self):
         """
         Creates a new batch called 'test'
         :return:
         """
         try:
-            add_batch('test', None, None, os.listdir(os.path.join(BASE, 'batches/mutation_detection/data')), False, sys.stdout)
-        except Exception as x:
-            self.fail('Failed with error: ' + x.strerror)
+            # Run the command itself
+            with suppress_stdout():
+                self.create_test_batch()
+
+            # Assert
+            assert (os.path.exists(self.batch_groovy))
+            assert (os.path.exists(self.sample_metadata))
+            assert (os.stat(self.batch_groovy).st_size > 0)
+            assert (os.stat(self.sample_metadata).st_size > 0)
+
+            # Cleanup data
+            self.delete_test_batch()
+
+        except Exception:
+            self.fail('Failed with error: ' + traceback.format_exc())
+
+    def test_show_batches(self):
+        stream = StringIO.StringIO()
+        with suppress_stdout():
+            show_batches(stream)
+        assert (len(stream.getvalue()) > 0)
+
+    def test_show_batch(self):
+        self.create_test_batch()
+        stream = StringIO.StringIO()
+        with suppress_stdout():
+            show_batch(self.test_batch_name, stream)
+        assert (len(stream.getvalue()) > 0)
+        self.delete_test_batch()
+
+    def test_add_sample(self):
+        self.create_test_batch()
+        initial_size = os.stat(self.sample_metadata).st_size
+        with suppress_stdout():
+            add_sample(self.test_batch_name, 'ALL', self.test_data_files, sys.stdout)
+        final_size = os.stat(self.sample_metadata).st_size
+        self.delete_test_batch()
+        assert(final_size > initial_size)
